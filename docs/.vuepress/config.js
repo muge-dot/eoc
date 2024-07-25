1bbcf6fbc2fa9627b491f4e3ccb244252f429b1f
Muge-w <2152929989@qq.com>
2024-07-25 20:03:59 +0800

    thing

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,8 +5,4 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
   bundler: viteBundler(),
   theme: defaultTheme(),
-
-  lang: 'zh-CN',
-  title: 'EOC',
-  description: '这是我的第一个 VuePress 站点',
 })
