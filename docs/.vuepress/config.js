8e35bd94f43fcd2761b45d96f5b3422a8359096f
Muge-w <2152929989@qq.com>
2024-07-25 19:34:51 +0800

    nothing

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  bundler: viteBundler(),
+  theme: defaultTheme(),
+
+  lang: 'zh-CN',
+  title: 'EOC',
+  description: '这是我的第一个 VuePress 站点',
+})
