e62b7206bd7aff5eb5892cbbf21e22056c4fb220
Muge-w <2152929989@qq.com>
2024-07-26 12:06:40 +0800

    nothing

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,8 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
+  base: "/eoc/",
+
   bundler: viteBundler(),
   theme: defaultTheme(),
 })
